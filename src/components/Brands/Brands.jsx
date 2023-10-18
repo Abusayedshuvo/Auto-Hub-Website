0622f5df8966cd30e49f0dc9049373a7064cc226
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2023-10-18 21:48:01 +0600

    added brands section on home page

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,54 @@
+const Brands = () => {
+  const brands = [
+    {
+      id: 1,
+      name: "Toyota",
+      img: "https://i.ibb.co/sqwTmTm/toyota.jpg",
+    },
+    {
+      id: 2,
+      name: "Audi",
+      img: "https://i.ibb.co/K6KxGDz/audi.jpg",
+    },
+    {
+      id: 3,
+      name: "BMW",
+      img: "https://i.ibb.co/CMxmCRp/bmw.jpg",
+    },
+    {
+      id: 4,
+      name: "Mercedes-Benz",
+      img: "https://i.ibb.co/BzHvfYz/mercedes.jpg",
+    },
+    {
+      id: 5,
+      name: "Tesla",
+      img: "https://i.ibb.co/THGvM5J/tesla.jpg",
+    },
+    {
+      id: 6,
+      name: "Honda",
+      img: "https://i.ibb.co/Qky9B1K/honda.jpg",
+    },
+  ];
+  return (
+    <>
+      <div className="container mx-auto md:my-10 lg:my-20">
+        <p className="text-6xl mb-20 text-center font-racing"> Our Brands</p>
+
+        <div className="grid grid-cols-3 gap-6 ">
+          {brands.map((brand) => (
+            <div className="bg-slate-200" key={brand.id}>
+              <img className="bor" src={brand.img} alt="" />
+              <p className="text-3xl font-semibold text-center p-4">
+                {brand.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Brands;
