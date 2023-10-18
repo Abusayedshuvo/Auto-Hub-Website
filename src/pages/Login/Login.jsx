ec68914f9a3079ef1e648cde956ed6715a46dbbc
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2023-10-18 14:26:37 +0600

    Login with google done on login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { FaGoogle, FaGithub, FaFacebookF } from "react-icons/fa6";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthProvider, { AuthContext } from "../../context/AuthProvider";
 
 const Login = () => {
   const { googleLogin } = useContext(AuthContext);
+  const [error, setError] = useState("");
+
+  const handleGoogle = () => {
+    googleLogin()
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+      });
+  };
   return (
     <div className="bg-[url('/src/assets/slide1.jpg')] h-screen flex justify-center items-center font-montserrat">
       <div className="text-center bg-white/90 p-10 rounded-md lg:w-4/12">
@@ -49,13 +61,16 @@ const Login = () => {
           <p className="my-5">Or Login With</p>
 
           <div className="space-x-4">
-            <button className="btn bg-[#EA4335] text-white capitalize">
+            <button
+              onClick={handleGoogle}
+              className="btn bg-[#EA4335] hover:text-[#EA4335] text-white capitalize"
+            >
               <FaGoogle /> Google
             </button>
-            <button className="btn bg-[#333] text-white capitalize">
+            <button className="btn bg-[#333] hover:text-[#333] text-white capitalize">
               <FaGithub /> Github
             </button>
-            <button className="btn bg-[#3b5998] text-white capitalize">
+            <button className="btn bg-[#3b5998] hover:text-[#3b5998] text-white capitalize">
               <FaFacebookF /> Facebook
             </button>
           </div>
